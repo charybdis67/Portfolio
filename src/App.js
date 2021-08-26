ea44170f1adc6cc29eade4b912206b203bfada2f
Asuman Aydın <asuman.aydin6709@gmail.com>
2021-08-26 12:25:48 +0300

    başlangıç taslak v1.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import background from "./img/modern-6097781_1920.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="flex">
+        <div className="navbar">
+          {" "}
+          <b className="list">About Me</b>
+          <b className="list">Projects</b>
+          <b className="list">Blog</b>
+          <b className="list">Contact</b>
+        </div>
+      </nav>
+      <div className="image"></div>
+      <h1 className="header">ASUMAN AYDIN</h1>
+    </div>
+  );
+}
+
+export default App;
